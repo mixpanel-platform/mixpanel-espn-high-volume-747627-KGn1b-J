57f8a8867e065d58ab17be18565f972ad775a43f
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2016-05-02 15:08:39 -0600

    temp fix for NaN%

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ function displayTable(div, tableArray, event) {
 
 function addRow(tableDiv, data, header) {
   header = header ? header : '';
-  if (!data.change) { data.change = '3%'; }
+  if (!data.change) { data.change = '3'; }
   var change = header ? '' : '%';
   var color = header ? '' : ' style="color: ' + colorFormat(data.change) + ';"';
   $('<div class="row ' + header + '">' + 
